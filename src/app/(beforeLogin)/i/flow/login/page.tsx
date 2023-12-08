aa001ad12ffefb72d556ae5fed96a33f103c2cd4
hyzzzzy <shinhj4068@naver.com>
2023-12-08 21:49:27 +0900

    feat: 병렬 라우팅 적용하여 로그인 모달 구현

--- src/app/(beforeLogin)/i/flow/login/page.tsx
+++ src/app/(beforeLogin)/i/flow/login/page.tsx
@@ -1,9 +1,7 @@
-import styles from './page.module.css'
-
 export default function Home() {
   return (
-    <main className={styles.main}>
-      로그인 페이지
-    </main>
+    <div>
+      로그인 모달
+    </div>
   )
 }
