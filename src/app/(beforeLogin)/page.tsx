ce5ebc9bcfbeed32b14c06c91aace43071e6824b
hyzzzzy <shinhj4068@naver.com>
2023-12-08 22:36:22 +0900

    rename: 인터셉팅 라우트 설정으로 의도한 로그인 모달 구현 및 폴더구조 변경

--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -14,7 +14,8 @@ export default function Home() {
         <h2>지금 가입하세요.</h2>
         <Link href='/i/flow/signup' className={styles.signup}>계정 만들기</Link>
         <h3>이미 트위터에 가입하셨나요?</h3>
-        <Link href='/login' className={styles.login}>로그인</Link>
+        {/* TODO: 로그인 리다이렉트 다시 /login 으로 변경해야 함 */}
+        <Link href='/i/flow/login' className={styles.login}>로그인</Link>
       </div>
     </>
   )
