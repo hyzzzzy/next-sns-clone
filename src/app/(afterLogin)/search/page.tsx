44f23d19def8ee2d2bbcb0a70925c9bd6b7d0585
hyzzzzy <shinhj4068@naver.com>
2023-12-27 17:44:15 +0900

    feat(search): 검색 결과 페이지 구현

--- src/app/(afterLogin)/search/page.tsx
+++ src/app/(afterLogin)/search/page.tsx
@@ -1,5 +1,45 @@
-export default function Home() {
+import style from './search.module.css';
+import BackButton from "@/app/(afterLogin)/_component/BackButton";
+import SearchForm from "@/app/(afterLogin)/_component/SearchForm";
+import Tab from "@/app/(afterLogin)/search/_component/Tab";
+import Post from "@/app/(afterLogin)/_component/Post";
+
+type Props = {
+  /**
+   * q: 검색어
+   * f: 탭의 값 (ex. 최신 탭일 때, f=live)
+   * pf: 검색필터 (ex. 내가 팔로우 한 사용자의 게시글 검색일 때 pf=on)
+   */
+  searchParams: { q: string, f?: string, pf?: string };
+}
+export default function Search({ searchParams }: Props) {
   return (
-    <div>검색</div>
+    <main className={style.main}>
+      <div className={style.searchTop}>
+        <div className={style.searchZone}>
+          <div className={style.buttonZone}>
+            <BackButton/>
+          </div>
+          <div className={style.formZone}>
+            <SearchForm q={searchParams.q} />
+          </div>
+        </div>
+        <Tab/>
+      </div>
+      <div className={style.list}>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        {/*<SearchResult searchParams={searchParams} />*/}
+      </div>
+    </main>
   )
 }
